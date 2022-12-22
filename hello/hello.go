0d89debdb94f5bfcada93e27303d702395e1d4d6
舞羡 <yatao.tyt@antgroup.com>
2022-12-22 15:29:42 +0800

    Call your code from another module

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Opt())
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
