4c74d7f8fa474b9661df41070c98c405d85d4b1d
Hinsteny <hinsteny@gmail.com>
2022-12-31 19:01:59 +0800

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
@@ -9,7 +10,34 @@ import (
 
 func main() {
 	fmt.Println(quote.Opt())
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("Hinsteny")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
+
+	// Request a greeting message.
+	message1, err1 := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message1)
 }
